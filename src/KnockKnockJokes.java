3e9e6a9ebb40d064672433d1dd71febc73c9b21f
angelinevillachica <league@WorthyMini.local>
2017-05-13 11:26:36 -0700

    we made computers say jokes and we gave wendy the zombie instructions

---
+++ src/KnockKnockJokes.java
@@ -0,0 +1,31 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnockJokes {
+public static void main(String[] args) {
+JOptionPane.showInputDialog("Knock Knock");	
+JOptionPane.showInputDialog("isabell");	
+JOptionPane.showInputDialog("isabell working i had to knock");	
+JOptionPane.showInputDialog("knock knock");	
+JOptionPane.showInputDialog("Annie");	
+JOptionPane.showInputDialog("Annie body home?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ src/MorningZombie.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+System.out.println("i'm tired and sick");
+JOptionPane.showMessageDialog(null, "Hello Wendy theese are the steps you need to do to be like a normal boring person");
+JOptionPane.showMessageDialog(null,"Step 1: Wake up and change your clothes");
+JOptionPane.showMessageDialog(null,"Step 2:Do your hair");
+JOptionPane.showMessageDialog(null,"Step 3:Eat breakfeast");
+JOptionPane.showMessageDialog(null,"Step 4:Brush your teeth");
+JOptionPane.showMessageDialog(null,"Step 5:Get your Backpack");
+JOptionPane.showMessageDialog(null,"Step 6: Go to school.Have fun!");
+JOptionPane.showMessageDialog(null,"REMEMBER DO NOT EAT ANYBODY AND THEIR BRAINS");
+
+
+
+
+
+
+
+
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
