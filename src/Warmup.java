de0cc7f23aaa4f3ee90c1efa6b71372078485972
joshpryor <league@Admins-Mac-mini-2.local>
2017-05-27 11:26:28 -0700

    i made computers say riddles

---
+++ src/Warmup.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Hello person");
+		JOptionPane.showMessageDialog(null, "I like wolves");
+		JOptionPane.showMessageDialog(null, "I like dragons");
+		JOptionPane.showMessageDialog(null, "I love my dog Joy");
+		JOptionPane.showMessageDialog(null, "I like harry potter");
+		JOptionPane.showMessageDialog(null, "I like sleeping and eating popcorn and watching you tube");
+	
+	
+	
+	
+	}
+}
---
+++ src/riddler.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class riddler {
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog(
+				"There is a woman in her hotel room.There is a knock on the door.The woman opens the door and there is a man.The man says sorry i thought this was my room.The man left and the woman locked the door and called security.What made the woman call security?");
+		JOptionPane.showMessageDialog(null, "you don't knock on your hotel room door");
+		JOptionPane.showInputDialog(
+				"A king has no sons, no daughters, and no queen. For this reason he must decide who will take the throne after he dies. To do this he decides that he will give all of the children of the kingdom a single seed. Whichever child has the largest, most beautiful plant will earn the throne; this being a metaphor for the kingdom.At the end of the contest all of the children came to the palace with their enormous and beautiful plants in hand. After he looks at all of the children's pots, he finally decides that the little girl with an empty pot will be the next Queen. Why did he choose this little girl over all of the other children with their beautiful plants?");
+		JOptionPane.showMessageDialog(null,
+				"The king gave them all fake seeds and the little girl was the only honest child who didn't switch seeds.");
+	JOptionPane.showInputDialog("Feed me and I live, yet give me a drink and I die");
+	JOptionPane.showMessageDialog(null, "Fire");
+	
+	
+	
+	}
+}
