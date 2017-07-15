34442261c454a36758a6e0e92a3ccf9349618606
angelinevillachica <league@Mac-mini-4.local>
2017-07-15 11:35:02 -0700

    we made creepy code

---
+++ src/Creeper.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Creeper {
+public static void main(String[] args) {
+String name=JOptionPane.showInputDialog("Hi my name is  haley and im calling you to inform you that you haven't payed your vet bills from petsmart and what is your full name");
+String adressphone=JOptionPane.showInputDialog("So you can start paying your bills right now in a much easier fashion i will need your phone number and adress to start and your social security number");
+String creditcardnumberpin=JOptionPane.showInputDialog("ok i have logged into your account so i can start paying your bill. I also need your credit card number,pin, and birthday");
+JOptionPane.showInputDialog("so this is all correct ..."+name+adressphone+creditcardnumberpin);
+JOptionPane.showInputDialog("thank you for all your information you actually haven't payed your bills and now i will hack your bank acount thank you for your time bye");
+
+
+
+
+
+
+
+
+
+
+}
+}
---
+++ src/Joy.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class Joy {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "hi");
+String name=JOptionPane.showInputDialog("what is your name");
+JOptionPane.showMessageDialog(null, "hello"+name);
+String favorite=JOptionPane.showInputDialog("what is your favorite thing");
+JOptionPane.showMessageDialog(null, favorite+ "are awesome");
+
+
+
+
+
+
+}
+}
